05d2cb1d44cb9445226e8351e2628879b38059a6
Nkugavarathan <kugavarathan28@gmail.com>
2025-05-06 16:26:26 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,145 +1,38 @@
-import { Link } from "react-scroll"
-// import React from "react"
+// Footer.js
+import { Container, Row, Col } from "react-bootstrap"
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa"
 
 function Footer() {
   return (
-    <footer className="footer--container bg-primary-subtle text-dark py-4">
-      <div className="container d-flex justify-content-center align-items-center">
-        <div className="row text-center w-100">
-          {/* Logo Section */}
-          <div className="col-12 col-md-3 mb-3 mb-md-0">
-            <h5>KSU Graphic Logo</h5>
-          </div>
-
-          {/* Navigation Links - Horizontal on Desktop, Vertical on Mobile */}
-          <div className="col-12 col-md-6">
-            <ul className="list-unstyled d-flex flex-column flex-md-row justify-content-center align-items-center">
-              <li className="mb-2 mb-md-0 mx-md-2">
-                <Link
-                  activeClass="navbar--active-content"
-                  spy={true}
-                  smooth={true}
-                  offset={-70}
-                  duration={500}
-                  to="heroSection"
-                  className="footer-link"
-                >
-                  Home
-                </Link>
-              </li>
-              <li className="mb-2 mb-md-0 mx-md-2">
-                <Link
-                  activeClass="navbar--active-content"
-                  spy={true}
-                  smooth={true}
-                  offset={-70}
-                  duration={500}
-                  to=""
-                  className="footer-link"
-                >
-                  Terms & Conditions
-                </Link>
-              </li>
-
-              <li className="mb-2 mb-md-0 mx-md-2">
-                <Link
-                  activeClass="navbar--active-content"
-                  spy={true}
-                  smooth={true}
-                  offset={-70}
-                  duration={500}
-                  to="Contact"
-                  className="footer-link"
-                >
-                  Privacy Policy
-                </Link>
-              </li>
-              <li className="mb-2 mb-md-0 mx-md-2">
-                <Link
-                  activeClass="navbar--active-content"
-                  spy={true}
-                  smooth={true}
-                  offset={-70}
-                  duration={500}
-                  to="Contact"
-                  className="footer-link"
-                >
-                  Contact
-                </Link>
-              </li>
-              {/* <li className="mb-2 mb-md-0 mx-md-2">
-                <Link
-                  activeClass="navbar--active-content"
-                  spy={true}
-                  smooth={true}
-                  offset={-70}
-                  duration={500}
-                  to="testimonial"
-                  className="footer-link"
-                >
-                  Testimonials
-                </Link>
-              </li> */}
-            </ul>
-          </div>
-
-          {/* Social Media Icons */}
-          <div className="col-12 col-md-3">
-            <ul className="list-inline m-0">
-              <li className="list-inline-item mx-2">
-                <a
-                  href="https://www.instagram.com/"
-                  className="social-icon"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <i className="fab fa-instagram fa-lg"></i>
-                </a>
-              </li>
-              <li className="list-inline-item mx-2">
-                <a
-                  href="https://www.facebook.com/"
-                  className="social-icon"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <i className="fab fa-facebook fa-lg"></i>
-                </a>
-              </li>
-              <li className="list-inline-item mx-2">
-                <a
-                  href="https://www.linkedin.com/"
-                  className="social-icon"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <i className="fab fa-linkedin fa-lg"></i>
-                </a>
-              </li>
-              <li className="list-inline-item mx-2">
-                <a
-                  href="https://discord.com/"
-                  className="social-icon"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <i className="fab fa-discord fa-lg"></i>
-                </a>
-              </li>
-              <li className="list-inline-item mx-2">
-                <a
-                  href="https://twitter.com/"
-                  className="social-icon"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <i className="fab fa-x-twitter fa-lg"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+    <footer className="bg-dark text-white py-4">
+      <Container>
+        <Row>
+          <Col md={6}>
+            <h5>KSU Graphics</h5>
+          </Col>
+          <Col md={6} className="d-flex justify-content-end align-items-center">
+            <div className="me-3">
+              <a href="/" className="text-white me-3">
+                Home
+              </a>
+              <a href="/" className="text-white me-3">
+                Terms
+              </a>
+              <a href="/" className="text-white me-3">
+                Privacy
+              </a>
+              <a href="/" className="text-white">
+                Contact
+              </a>
+            </div>
+            <div>
+              <FaFacebook className="me-2" />
+              <FaInstagram className="me-2" />
+              <FaYoutube />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   )
 }
