283acba14800fe0000ba0ff3dc6274deacd0914f
Nkugavarathan <kugavarathan28@gmail.com>
2025-05-08 11:43:15 +0530

    about updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,24 +1,27 @@
 import React from "react"
-// import aboutImg from "../assets/about.jpg" // use your image path
-import { Container, Row, Col } from "react-bootstrap"
+// import aboutImg from "../assets/about.jpg" // Make sure this path is correct
 
 function About() {
   return (
-    <Container className="my-5">
-      <Row className="align-items-center">
-        <Col md={6}>
-          <h2>About Us</h2>
-          <p>
+    <div className="container my-5">
+      <div className="row align-items-center">
+        <div className="col-md-6 mb-4 mb-md-0">
+          <h2 className="mb-3">About Us</h2>
+          <p className="text-muted">
             We are a printing company providing high-quality digital and offset
             printing services. Our mission is to bring your ideas to life with
             precision and passion.
           </p>
-        </Col>
-        <Col md={6}>
-          {/* <img src={} alt="About us" className="img-fluid rounded" /> */}
-        </Col>
-      </Row>
-    </Container>
+        </div>
+        <div className="col-md-6">
+          <img
+            // src={aboutImg}
+            alt="About us"
+            className="img-fluid rounded shadow"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
