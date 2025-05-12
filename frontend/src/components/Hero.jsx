866b98d642649f9a1af81bc4e1421599d9d01595
Nkugavarathan <kugavarathan28@gmail.com>
2025-05-12 13:46:56 +0530

    update hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,8 +2,14 @@ import "./Hero.css"
 
 function Hero() {
   return (
-    <div className="hero-section d-flex align-items-center justify-content-center">
-      <h1 className="hero-text">KSU Graphic</h1>
+    <div className="hero-section d-flex align-items-center justify-content-center flex-column text-center">
+      <div className="hero-text-slider">
+        <div className="slider-text">
+          Digital Printing • Offset Printing • Digital Printing • Offset
+          Printing •
+        </div>
+      </div>
+      <h1 className="hero-main-text mt-4">KSU Graphic</h1>
     </div>
   )
 }
